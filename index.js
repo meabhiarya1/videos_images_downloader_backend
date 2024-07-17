e4c162fb0b10585337787b4c1b6618f5a7170130
Abhishek kumar <abhi.arya97161@gmail.com>
2024-07-17 17:06:19 +0530

    spinner added

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const youtubedl = require("youtube-dl-exec"); // Import youtube-dl-exec
-const fs = require("fs");
 const path = require("path");
 const app = express();
 const port = 8080;
--- routes/downloadRoutes.js
+++ routes/downloadRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { downloadController, deleteController, mediator } = require("../controller/downloadController");
+const {
+  downloadController,
+  deleteController,
+} = require("../controller/downloadController");
 
 router.post("/download-video", downloadController);
 router.post("/delete-video", deleteController);
 
 module.exports = router;
-    
